4835e7e02f30529d814f90682b00e8d21006d320
amerilain <amerilain@gmail.com>
2022-12-07 14:32:35 +0200

    Fixed error in previous commit
    temp in marker works
    
    comments added in game.py to win game

--- v.2/Backend-Python/game.py
+++ v.2/Backend-Python/game.py
@@ -45,9 +45,8 @@ class Game:
             print(sql2)
             cur2 = config.conn.cursor()
             cur2.execute(sql2)
-            # find game from DB
             # sql statement to check if game is won
-            #     if >= 8:
+            #     if count goalid >= 8:
             #        self.gameover = True
             # find game from DB
             sql = "SELECT id, co2_consumed, co2_budget, location, screen_name FROM Game WHERE id='" + id + "'"
@@ -64,8 +63,7 @@ class Game:
                         "consumed": res[0][1],
                         "budget": res[0][2]
                     },
-                    "previous_location": res[0][3],
-                    "game_status": self.gameover
+                    "previous_location": res[0][3]
                 }
                 # old location in DB currently not used
                 apt = Airport(loc, True)
@@ -75,40 +73,39 @@ class Game:
             else:
                 print("************** GAME NOT FOUND! ***************")
 
-    # read game's goals
+        # read game's goals
         self.fetch_goal_info()
 
+    def set_location(self, sijainti):
+        # self.location = sijainti
+        sql = "UPDATE Game SET location='" + sijainti.ident + "' WHERE id='" + self.status["id"] + "'"
+        print(sql)
+        cur = config.conn.cursor()
+        cur.execute(sql)
+        # config.conn.commit()
+        # self.loc = sijainti.ident
 
-def set_location(self, sijainti):
-    # self.location = sijainti
-    sql = "UPDATE Game SET location='" + sijainti.ident + "' WHERE id='" + self.status["id"] + "'"
-    print(sql)
-    cur = config.conn.cursor()
-    cur.execute(sql)
-    # config.conn.commit()
-    # self.loc = sijainti.ident
+    def fetch_goal_info(self):
 
+        sql = "SELECT * FROM (SELECT Goal.id, Goal.name, Goal.description, Goal.icon, GoalReached.gameid, "
+        sql += "Goal.target, Goal.target_minvalue, Goal.target_maxvalue, Goal.target_text "
+        sql += "FROM Goal INNER JOIN GoalReached ON Goal.id = GoalReached.goalid "
+        sql += "WHERE GoalReached.gameid = '" + self.status["id"] + "' "
+        sql += "UNION SELECT Goal.id, Goal.name, Goal.description, Goal.icon, NULL, "
+        sql += "Goal.target, Goal.target_minvalue, Goal.target_maxvalue, Goal.target_text "
+        sql += "FROM Goal WHERE Goal.id NOT IN ("
+        sql += "SELECT Goal.id FROM Goal INNER JOIN GoalReached ON Goal.id = GoalReached.goalid "
+        sql += "WHERE GoalReached.gameid = '" + self.status["id"] + "')) AS t ORDER BY t.id;"
 
-def fetch_goal_info(self):
-    sql = "SELECT * FROM (SELECT Goal.id, Goal.name, Goal.description, Goal.icon, GoalReached.gameid, "
-    sql += "Goal.target, Goal.target_minvalue, Goal.target_maxvalue, Goal.target_text "
-    sql += "FROM Goal INNER JOIN GoalReached ON Goal.id = GoalReached.goalid "
-    sql += "WHERE GoalReached.gameid = '" + self.status["id"] + "' "
-    sql += "UNION SELECT Goal.id, Goal.name, Goal.description, Goal.icon, NULL, "
-    sql += "Goal.target, Goal.target_minvalue, Goal.target_maxvalue, Goal.target_text "
-    sql += "FROM Goal WHERE Goal.id NOT IN ("
-    sql += "SELECT Goal.id FROM Goal INNER JOIN GoalReached ON Goal.id = GoalReached.goalid "
-    sql += "WHERE GoalReached.gameid = '" + self.status["id"] + "')) AS t ORDER BY t.id;"
-
-    print(sql)
-    cur = config.conn.cursor()
-    cur.execute(sql)
-    res = cur.fetchall()
-    for a in res:
-        if a[4] == self.status["id"]:
-            is_reached = True
-        else:
-            is_reached = False
-        goal = Goal(a[0], a[1], a[2], a[3], is_reached, a[5], a[6], a[7], a[8])
-        self.goals.append(goal)
-    return
+        print(sql)
+        cur = config.conn.cursor()
+        cur.execute(sql)
+        res = cur.fetchall()
+        for a in res:
+            if a[4] == self.status["id"]:
+                is_reached = True
+            else:
+                is_reached = False
+            goal = Goal(a[0], a[1], a[2], a[3], is_reached, a[5], a[6], a[7], a[8])
+            self.goals.append(goal)
+        return
